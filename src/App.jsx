4030c5ad1e169d6ecdbc65a30ec78729e169da55
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2025-12-09 16:22:19 +0530

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,16 @@ const App = () => {
     <div>
       <h1 className="title">Hello, everyone!</h1>
       <p className="Subtitle">Welcome to my React application.</p>
+      <img
+        className="image"
+        src="/About.png"
+        alt="Placeholder"
+      />
+      <img
+        className="image"
+        src="/2.png"
+        alt="Placeholder"
+      />
     </div>
   );
 };
